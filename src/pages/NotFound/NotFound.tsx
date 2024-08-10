75582193027d2b8086b5628e40dae7890ebcbe97
Eric <ericgeorge4477@gmail.com>
2024-08-10 16:23:56 +0300

    Complete NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="not-found-page h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl text-center">404 Not Found</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 
+      rounded focus:outline-none focus:ring-2 focus:ring-blue-300 transition duration-300 ease-in-out"
+        onClick={() => navigate("/dashboard")}
+      >
+        Go Back
+      </button>
+    </div>
+  );
 }
 
 export default NotFound;
